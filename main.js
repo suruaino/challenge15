08258d20d12f5e83a400df05119eedf9b8358c51
SURU EMMANUEL <suruaino@gmail.com>
2024-03-09 10:48:33 +0100

    Started work on js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let summaryScreen = document.getElementById('screen');
+let toggleBar = document.getElementById("toggle");
 
 summaryScreen.innerText = "Weldon bro, This is your summary page!";
+
+function toggled(){
+    let toggleSpan = document.getElementById("toggle_span");
+    toggleSpan.style.right = 0;
+}
+
