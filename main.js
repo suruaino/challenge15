42eeee16d9bad54e7c2acd1fd7a87a1a72b18560
SURU EMMANUEL <suruaino@gmail.com>
2024-03-10 17:19:47 +0100

    debugged the toggle btn

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-let summaryScreen = document.getElementById('screen');
+document.addEventListener('DOMContentLoaded', function() {
+let summaryScreen = document.getElementById("screen");
 let toggleBar = document.getElementById("toggle");
 
 let planBx = document.querySelectorAll(".select_box");
 
 planBx.forEach((container, index) => {
-    const radio = container.querySelector('input[type="radio"]');
-    
-    container.addEventListener('click', () => {
-        radio.checked = true;
-        // container.style.backgroundColor = "red"
-    });
+  const radio = container.querySelector('input[type="radio"]');
+
+  container.addEventListener("click", () => {
+    radio.checked = true;
+    // container.style.backgroundColor = "red"
+  });
 });
 
 summaryScreen.innerText = "Weldon bro, This is your summary page!";
 
-function toggled(){
-    let toggleSpan = document.getElementById("toggle_span");
-    toggleSpan.style.right = 0;
+let toggleSpan = document.getElementById("toggle_span");
+toggleSpan.style.backgroundColor = "red"
+let toggle = document.getElementById("toggle_input");
+toggle.checked = true;
+if (toggle.checked) {
+  toggleSpan.style.right = 0;
+} else {
+     toggleSpan.style.left = 0; 
 }
 
+})
