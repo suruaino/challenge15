3485e1213da9490a3f673f634a448be3b4bb894b
SURU EMMANUEL <suruaino@gmail.com>
2024-03-10 15:51:19 +0100

    Added radio button the plan section

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let summaryScreen = document.getElementById('screen');
 let toggleBar = document.getElementById("toggle");
 
+
+let arcade = document.getElementById("arcade");
+
+arcade.addEventListener("click", function(){
+    document.querySelector('input[type="radio"]').checked = "true"
+}) 
 summaryScreen.innerText = "Weldon bro, This is your summary page!";
 
 function toggled(){
