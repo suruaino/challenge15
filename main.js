94940c9a60dcf3fa6cef0bb54f840221ea1ddcef
SURU EMMANUEL <suruaino@gmail.com>
2024-03-11 19:13:55 +0100

    added switch between month and year of add-on section

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ let monthly = document.getElementById("monthly");
 let yearly = document.getElementById("yearly");
 let monthlyPlan = document.getElementById("plan_monthly");
 let yearlyPlan = document.getElementById("plan_yearly");
+let monthlyAdd = document.getElementById("add_monthly");
+let yearlyAdd = document.getElementById("add_yearly");
 let checkInput = document.getElementById("toggle_input");
 
 checkInput.addEventListener("change", function () {
@@ -24,6 +26,8 @@ checkInput.addEventListener("change", function () {
   yearly.style.color = this.checked ? "#02295a" : "#9699ab";
   monthlyPlan.style.display = this.checked ? "none" : "flex";
   yearlyPlan.style.display = this.checked ? "flex" : "none";
+  monthlyAdd.style.display = this.checked ? "none" : "flex";
+  yearlyAdd.style.display = this.checked ? "flex" : "none";
 });
 
 // })
